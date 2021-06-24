1a3ed4b2ae66846bbc08f72498b6e78293e8d61c
Leonardo Pinheiro <leoprocha100@gmail.com>
2021-06-24 20:21:23 -0300

    feat(core): configure build output for all modern front-end frameworks

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -1,8 +1,24 @@
 import { Config } from '@stencil/core'
 
+import { angularOutputTarget } from '@stencil/angular-output-target'
+import { reactOutputTarget } from '@stencil/react-output-target'
+import { vueOutputTarget } from '@stencil/vue-output-target'
+
 export const config: Config = {
   namespace: 'plug-checkout',
   outputTargets: [
+    angularOutputTarget({
+      componentCorePackage: '@plug-checkout/core',
+      directivesProxyFile: '../angular/src/directives/proxies.ts',
+    }),
+    reactOutputTarget({
+      componentCorePackage: '@plug-checkout/core',
+      proxiesFile: '../react/src/components.ts',
+    }),
+    vueOutputTarget({
+      componentCorePackage: '@plug-checkout/core',
+      proxiesFile: '../vue/src/proxies.ts',
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',

516b7712a5938614004e689c552dcde8b586e8bc
Leonardo Pinheiro <leoprocha100@gmail.com>
2021-06-24 19:56:31 -0300

    feat(vue): adding vue package

---
+++ packages/vue/setupTests.ts
@@ -0,0 +1,20 @@
+window.matchMedia =
+  window.matchMedia ||
+  (function () {
+    return {
+      matches: false,
+      addListener() {},
+      removeListener() {},
+    }
+  } as any)
+;(window as any).CSSStyleSheet = class {
+  constructor() {
+    throw Error()
+  }
+} as any
+
+let error = console.error
+console.error = function (message: any) {
+  error.apply(console, arguments)
+  throw message instanceof Error ? message : new Error(message)
+}

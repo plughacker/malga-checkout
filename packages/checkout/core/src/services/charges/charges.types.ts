e4f73a6b4c3dd683c439127e3990b87ce05c1036
Filippe Rodrigues <filipperodrigues@rede.ulbra.br>
2022-10-31 11:19:54 -0300

    fix(payment-dto): fixed payment session endpoint payload data

--- packages/checkout/core/src/services/charges/charges.types.ts
+++ packages/checkout/core/src/services/charges/charges.types.ts
@@ -14,6 +14,7 @@ export interface CreateChargeData {
   statementDescriptor?: string
   capture?: boolean
   description?: string
+  fraudAnalysis?: FraudAnalysis
 }
 
 export interface FraudAnalysisCart {

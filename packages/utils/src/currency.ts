d9f0af7ea440b2ebda48958ff1420dfe24289f14
Leonardo Pinheiro <leoprocha100@gmail.com>
2022-11-04 15:35:42 -0300

    fix(utils): currencies without decimal

--- packages/utils/src/currency.ts
+++ packages/utils/src/currency.ts
@@ -2,12 +2,45 @@ export const formatCurrency = (
   value: number,
   currencyFormat: string | undefined = 'BRL',
 ) => {
-  const centsToReal = value / 100
+  const currenciesWithoutDecimal = [
+    'YER',
+    'AFN',
+    'ALL',
+    'BIF',
+    'CLP',
+    'DJF',
+    'GNF',
+    'ISK',
+    'JPY',
+    'KMF',
+    'KRW',
+    'LAK',
+    'LBP',
+    'MGA',
+    'MMK',
+    'MRO',
+    'PYG',
+    'RSD',
+    'RWF',
+    'SLL',
+    'SOS',
+    'STD',
+    'UGX',
+    'VND',
+    'VUV',
+    'XAF',
+    'XOF',
+    'XPF',
+  ]
+
+  const currentValue = currenciesWithoutDecimal.includes(currencyFormat)
+    ? value
+    : value / 100
 
   const currency = new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: currencyFormat,
   })
 
-  return currency.format(centsToReal)
+  return currency.format(currentValue)
 }

3e2cd19b0214179b7c4fe5e42c6ffc1b6295cc2d
Leonardo Pinheiro <leoprocha100@gmail.com>
2022-11-17 12:11:20 -0300

    fix(checkout-react): update build files

---
+++ packages/checkout/react/scripts/fix-build-files/files/components.ts
@@ -0,0 +1,18 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib'
+
+import type { JSX } from '@plug-checkout/core'
+
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@plug-checkout/core/loader'
+
+applyPolyfills().then(() => defineCustomElements())
+
+export const PlugCheckout = /*@__PURE__*/ createReactComponent<
+  JSX.PlugCheckout,
+  HTMLPlugCheckoutElement
+>('plug-checkout')

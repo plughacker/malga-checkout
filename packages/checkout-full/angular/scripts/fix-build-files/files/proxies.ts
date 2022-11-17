e2e5015b8e4a92f6f9d3ce1e6fa76bb853e7d798
Leonardo Pinheiro <leoprocha100@gmail.com>
2022-11-17 12:36:49 -0300

    fix(checkout-full-angular): update build files with script

---
+++ packages/checkout-full/angular/scripts/fix-build-files/files/proxies.ts
@@ -0,0 +1,62 @@
+/* tslint:disable */
+/* eslint-disable */
+/* auto-generated angular directive proxies */
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  NgZone,
+} from '@angular/core'
+import { ProxyCmp, proxyOutputs } from './angular-component-lib/utils'
+
+import { Components } from '@plug-checkout-full/core'
+
+export declare interface PlugCheckoutFull extends Components.PlugCheckoutFull {}
+@ProxyCmp({
+  inputs: [
+    'clientId',
+    'sessionId',
+    'idempotencyKey',
+    'dialogConfig',
+    'merchantId',
+    'pageConfig',
+    'paymentMethods',
+    'publicKey',
+    'sandbox',
+    'debug',
+    'transactionConfig',
+  ],
+})
+@Component({
+  selector: 'plug-checkout-full',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: '<ng-content></ng-content>',
+  inputs: [
+    'clientId',
+    'sessionId',
+    'dialogConfig',
+    'idempotencyKey',
+    'merchantId',
+    'pageConfig',
+    'paymentMethods',
+    'publicKey',
+    'sandbox',
+    'debug',
+    'transactionConfig',
+  ],
+  outputs: ['transactionSuccess', 'transactionFailed'],
+})
+export class PlugCheckoutFull {
+  /**  */
+  transactionSuccess!: EventEmitter<CustomEvent<{ data: unknown }>>
+  /**  */
+  transactionFailed!: EventEmitter<CustomEvent<{ error: unknown }>>
+  protected el: HTMLElement
+  constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
+    c.detach()
+    this.el = r.nativeElement
+    proxyOutputs(this, this.el, ['transactionSuccess', 'transactionFailed'])
+  }
+}

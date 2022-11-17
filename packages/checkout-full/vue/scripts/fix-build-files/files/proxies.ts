65beb871690976a0e191792bdc7ea0a0aaf91f4c
Leonardo Pinheiro <leoprocha100@gmail.com>
2022-11-17 12:40:22 -0300

    fix(checkout-full-vue): update build files with script

---
+++ packages/checkout-full/vue/scripts/fix-build-files/files/proxies.ts
@@ -0,0 +1,27 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated vue proxies */
+import { defineContainer } from './vue-component-lib/utils'
+
+import type { JSX } from '@plug-checkout-full/core'
+
+export const PlugCheckoutFull =
+  /*@__PURE__*/ defineContainer<JSX.PlugCheckoutFull>(
+    'plug-checkout-full',
+    undefined,
+    [
+      'clientId',
+      'publicKey',
+      'sessionId',
+      'merchantId',
+      'idempotencyKey',
+      'sandbox',
+      'debug',
+      'paymentMethods',
+      'pageConfig',
+      'transactionConfig',
+      'dialogConfig',
+      'transactionSuccess',
+      'transactionFailed',
+    ],
+  )

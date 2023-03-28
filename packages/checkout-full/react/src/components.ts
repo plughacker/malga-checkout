38ef02284cc4875217e3207c68a4847eea1b73db
Leonardo Pinheiro <leoprocha100@gmail.com>
2023-03-28 14:35:37 -0300

    fix(checkout-full): remove unused changes

--- packages/checkout-full/react/src/components.ts
+++ packages/checkout-full/react/src/components.ts
@@ -1,44 +1,18 @@
 /* eslint-disable */
 /* tslint:disable */
 /* auto-generated react proxies */
-import { createReactComponent } from './react-component-lib';
+import { createReactComponent } from './react-component-lib'
 
-import type { JSX } from '@malga-checkout-full/core';
+import type { JSX } from '@malga-checkout-full/core'
 
-import { applyPolyfills, defineCustomElements } from '@malga-checkout-full/core/loader';
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@malga-checkout-full/core/loader'
 
-applyPolyfills().then(() => defineCustomElements());
-export const CheckoutAccordion = /*@__PURE__*/createReactComponent<JSX.CheckoutAccordion, HTMLCheckoutAccordionElement>('checkout-accordion');
-export const CheckoutButton = /*@__PURE__*/createReactComponent<JSX.CheckoutButton, HTMLCheckoutButtonElement>('checkout-button');
-export const CheckoutClipboardButton = /*@__PURE__*/createReactComponent<JSX.CheckoutClipboardButton, HTMLCheckoutClipboardButtonElement>('checkout-clipboard-button');
-export const CheckoutCountdown = /*@__PURE__*/createReactComponent<JSX.CheckoutCountdown, HTMLCheckoutCountdownElement>('checkout-countdown');
-export const CheckoutCreditCard = /*@__PURE__*/createReactComponent<JSX.CheckoutCreditCard, HTMLCheckoutCreditCardElement>('checkout-credit-card');
-export const CheckoutDropdown = /*@__PURE__*/createReactComponent<JSX.CheckoutDropdown, HTMLCheckoutDropdownElement>('checkout-dropdown');
-export const CheckoutErrorMessage = /*@__PURE__*/createReactComponent<JSX.CheckoutErrorMessage, HTMLCheckoutErrorMessageElement>('checkout-error-message');
-export const CheckoutIcon = /*@__PURE__*/createReactComponent<JSX.CheckoutIcon, HTMLCheckoutIconElement>('checkout-icon');
-export const CheckoutLoader = /*@__PURE__*/createReactComponent<JSX.CheckoutLoader, HTMLCheckoutLoaderElement>('checkout-loader');
-export const CheckoutManualPayment = /*@__PURE__*/createReactComponent<JSX.CheckoutManualPayment, HTMLCheckoutManualPaymentElement>('checkout-manual-payment');
-export const CheckoutModal = /*@__PURE__*/createReactComponent<JSX.CheckoutModal, HTMLCheckoutModalElement>('checkout-modal');
-export const CheckoutModalBoleto = /*@__PURE__*/createReactComponent<JSX.CheckoutModalBoleto, HTMLCheckoutModalBoletoElement>('checkout-modal-boleto');
-export const CheckoutModalError = /*@__PURE__*/createReactComponent<JSX.CheckoutModalError, HTMLCheckoutModalErrorElement>('checkout-modal-error');
-export const CheckoutModalPix = /*@__PURE__*/createReactComponent<JSX.CheckoutModalPix, HTMLCheckoutModalPixElement>('checkout-modal-pix');
-export const CheckoutModalSuccess = /*@__PURE__*/createReactComponent<JSX.CheckoutModalSuccess, HTMLCheckoutModalSuccessElement>('checkout-modal-success');
-export const CheckoutOrderSummary = /*@__PURE__*/createReactComponent<JSX.CheckoutOrderSummary, HTMLCheckoutOrderSummaryElement>('checkout-order-summary');
-export const CheckoutRadioField = /*@__PURE__*/createReactComponent<JSX.CheckoutRadioField, HTMLCheckoutRadioFieldElement>('checkout-radio-field');
-export const CheckoutSelectField = /*@__PURE__*/createReactComponent<JSX.CheckoutSelectField, HTMLCheckoutSelectFieldElement>('checkout-select-field');
-export const CheckoutSkeleton = /*@__PURE__*/createReactComponent<JSX.CheckoutSkeleton, HTMLCheckoutSkeletonElement>('checkout-skeleton');
-export const CheckoutSwitch = /*@__PURE__*/createReactComponent<JSX.CheckoutSwitch, HTMLCheckoutSwitchElement>('checkout-switch');
-export const CheckoutTextField = /*@__PURE__*/createReactComponent<JSX.CheckoutTextField, HTMLCheckoutTextFieldElement>('checkout-text-field');
-export const CheckoutTypography = /*@__PURE__*/createReactComponent<JSX.CheckoutTypography, HTMLCheckoutTypographyElement>('checkout-typography');
-export const MalgaCheckout = /*@__PURE__*/createReactComponent<JSX.MalgaCheckout, HTMLMalgaCheckoutElement>('malga-checkout');
-export const MalgaCheckoutFull = /*@__PURE__*/createReactComponent<JSX.MalgaCheckoutFull, HTMLMalgaCheckoutFullElement>('malga-checkout-full');
-export const MalgaCheckoutFullContent = /*@__PURE__*/createReactComponent<JSX.MalgaCheckoutFullContent, HTMLMalgaCheckoutFullContentElement>('malga-checkout-full-content');
-export const MalgaCheckoutFullFooter = /*@__PURE__*/createReactComponent<JSX.MalgaCheckoutFullFooter, HTMLMalgaCheckoutFullFooterElement>('malga-checkout-full-footer');
-export const MalgaCheckoutFullHeader = /*@__PURE__*/createReactComponent<JSX.MalgaCheckoutFullHeader, HTMLMalgaCheckoutFullHeaderElement>('malga-checkout-full-header');
-export const MalgaCheckoutFullIdentification = /*@__PURE__*/createReactComponent<JSX.MalgaCheckoutFullIdentification, HTMLMalgaCheckoutFullIdentificationElement>('malga-checkout-full-identification');
-export const MalgaPayments = /*@__PURE__*/createReactComponent<JSX.MalgaPayments, HTMLMalgaPaymentsElement>('malga-payments');
-export const MalgaPaymentsBoleto = /*@__PURE__*/createReactComponent<JSX.MalgaPaymentsBoleto, HTMLMalgaPaymentsBoletoElement>('malga-payments-boleto');
-export const MalgaPaymentsCredit = /*@__PURE__*/createReactComponent<JSX.MalgaPaymentsCredit, HTMLMalgaPaymentsCreditElement>('malga-payments-credit');
-export const MalgaPaymentsCreditForm = /*@__PURE__*/createReactComponent<JSX.MalgaPaymentsCreditForm, HTMLMalgaPaymentsCreditFormElement>('malga-payments-credit-form');
-export const MalgaPaymentsCreditSavedCards = /*@__PURE__*/createReactComponent<JSX.MalgaPaymentsCreditSavedCards, HTMLMalgaPaymentsCreditSavedCardsElement>('malga-payments-credit-saved-cards');
-export const MalgaPaymentsPix = /*@__PURE__*/createReactComponent<JSX.MalgaPaymentsPix, HTMLMalgaPaymentsPixElement>('malga-payments-pix');
+applyPolyfills().then(() => defineCustomElements())
+
+export const MalgaCheckoutFull = /*@__PURE__*/ createReactComponent<
+  JSX.MalgaCheckoutFull,
+  HTMLMalgaCheckoutFullElement
+>('malga-checkout-full')

73bc7f943a6f9d15830949d6564ba2ffd3a3fa3f
Leonardo Pinheiro <leoprocha100@gmail.com>
2023-07-24 15:39:38 -0300

    fix(checkout): adding close button on drip iframe

--- packages/checkout/core/src/components/malga-payments-drip/partials/malga-payments-drip-iframe/malga-payments-drip-iframe.tsx
+++ packages/checkout/core/src/components/malga-payments-drip/partials/malga-payments-drip-iframe/malga-payments-drip-iframe.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, ComponentInterface, State } from '@stencil/core'
+import { Component, Host, h, ComponentInterface } from '@stencil/core'
 
 import payment from '../../../../stores/payment'
 
@@ -7,37 +7,16 @@ import payment from '../../../../stores/payment'
   styleUrl: 'malga-payments-drip-iframe.scss',
 })
 export class MalgaPaymentsDripIframe implements ComponentInterface {
-  private iframe: HTMLIFrameElement = null
-
-  @State() loadCount = 0
-
-  componentDidLoad() {
-    if (this.iframe) {
-      this.iframe.addEventListener('load', () => {
-        this.loadCount = this.loadCount + 1
-      })
-    }
-  }
-
-  componentDidUpdate() {
-    // when the iframe redirects the user to another page, we will close the modal.
-    if (this.loadCount === 2) {
-      this.iframe.removeEventListener('load', () => null)
-      this.loadCount = 0
-      payment.paymentUrl = ''
-    }
+  private handleCloseIframe = () => {
+    payment.paymentUrl = ''
   }
 
   render() {
     return (
       <Host class={{ 'malga-payments-drip-iframe__container': true }}>
         <div class={{ 'malga-payments-drip-iframe__modal': true }}>
-          <iframe
-            ref={(element) => (this.iframe = element)}
-            src={payment.paymentUrl}
-            title="Drip"
-            frameBorder={0}
-          />
+          <button onClick={this.handleCloseIframe}>X</button>
+          <iframe src={payment.paymentUrl} title="Drip" frameBorder={0} />
         </div>
       </Host>
     )
